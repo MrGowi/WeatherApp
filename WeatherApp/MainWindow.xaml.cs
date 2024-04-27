3043e9253dbf2943f7649019db8a213f079d93a1
MrGowi <josegonzalezwillems@gmail.com>
2024-04-27 16:44:41 +0200

    Projektdateien hinzufügen.

---
+++ WeatherApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly string apiKey = "ad1b7fda11ae3d548c15e3c1911937eb";
+
+        private string requestUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            HttpClient httpClient = new HttpClient();
+
+            var city = "Basel";
+            var finalUri = requestUrl + "?q=" + city + "&appid=" + apiKey + "&units=metric";
+            HttpResponseMessage httpResponse = httpClient.GetAsync(finalUri).Result;
+
+            string response = httpResponse.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
